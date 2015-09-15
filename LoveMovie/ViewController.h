753527cfaa6b7592f7e3355c46f960e3bdecb50c
Clap <caishengwei@MacBook-Pro-2.local>
2015-09-15 20:53:51 +0800

    Initial Commit

---
+++ LoveMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoveMovie
+//
+//  Created by Clap on 15/9/15.
+//  Copyright (c) 2015年 蔡盛威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoveMovie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoveMovie
+//
+//  Created by Clap on 15/9/15.
+//  Copyright (c) 2015年 蔡盛威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
